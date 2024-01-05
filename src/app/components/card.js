7722c92caef5d4e6df861764786c81e8eb0d704f
Arman Abkar <armanabkar@gmail.com>
2024-01-05 11:31:05 +0330

    Add Gift Amounts

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 
-export default function Card({ gift }) {
+export default function Card({ gift, amounts }) {
   const capitalizeFirstWord = (word) => word[0].toUpperCase() + word.slice(1);
 
   return (
-    <p className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-      <h2 className={`mb-3 text-2xl font-semibold`}>
+    <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+      <p className={`mb-3 text-2xl font-semibold`}>
         {capitalizeFirstWord(gift)}
-      </h2>
+      </p>
+
       <Image
         src={`/images/${gift}.webp`}
         alt={gift}
@@ -18,32 +19,17 @@ export default function Card({ gift }) {
         style={{ width: "100%", height: "auto" }}
         quality={80}
       />
+
       <div className="grid grid-cols-4 gap-3 text-center">
-        <span className="border rounded border-gray-300 p-px bg-gray-100">
-          5$
-        </span>
-        <span className="border rounded border-gray-300 p-px bg-gray-100">
-          10$
-        </span>
-        <span className="border rounded border-gray-300 p-px bg-gray-100">
-          15$
-        </span>
-        <span className="border rounded border-gray-300 p-px bg-gray-100">
-          20$
-        </span>
-        <span className="border rounded border-gray-300 p-px bg-gray-100">
-          30$
-        </span>
-        <span className="border rounded border-gray-300 p-px bg-gray-100">
-          50$
-        </span>
-        <span className="border rounded border-gray-300 p-px bg-gray-100">
-          70$
-        </span>
-        <span className="border rounded border-gray-300 p-px bg-gray-100">
-          100$
-        </span>
+        {amounts.map((amount, i) => (
+          <p
+            className="border rounded border-gray-300 p-px bg-gray-100 dark:bg-neutral-800/30 dark:border-neutral-700 hover:bg-gray-300 hover:dark:bg-neutral-700"
+            key={i}
+          >
+            {amount}$
+          </p>
+        ))}
       </div>
-    </p>
+    </div>
   );
 }
