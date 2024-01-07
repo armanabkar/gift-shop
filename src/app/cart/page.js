326c950f2779c134315e125822ba07c48a6cb7aa
Arman Abkar <armanabkar@gmail.com>
2024-01-07 18:35:22 +0330

    Update Cart Screen styles

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,19 +1,19 @@
 export default function Cart() {
   return (
     <div className="grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-      <p className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex justify-between">
+      <p className="rounded border-gray-300 bg-gray-100 dark:bg-neutral-800/30 dark:border-neutral-700 hover:bg-gray-300 hover:dark:bg-neutral-700 px-5 py-4 transition-colors text-left flex justify-between">
         <span>30$ PlayStation Gift</span>
         <span className="text-rose-600 font-extrabold ">X</span>
       </p>
-      <p className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex justify-between">
+      <p className="rounded border-gray-300 bg-gray-100 dark:bg-neutral-800/30 dark:border-neutral-700 hover:bg-gray-300 hover:dark:bg-neutral-700 px-5 py-4 transition-colors text-left flex justify-between">
         <span>30$ Xbox Gift</span>
         <span className="text-rose-600 font-extrabold ">X</span>
       </p>
-      <p className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex justify-between">
+      <p className="rounded border-gray-300 bg-gray-100 dark:bg-neutral-800/30 dark:border-neutral-700 hover:bg-gray-300 hover:dark:bg-neutral-700 px-5 py-4 transition-colors text-left flex justify-between">
         <span>20$ Apple Gift</span>
         <span className="text-rose-600 font-extrabold ">X</span>
       </p>
-      <button className="group rounded-lg border border-gray-300 dark:border-neutral-700 px-5 py-4 transition-colors hover:bg-gray-300 hover:dark:border-neutral-700 text-left">
+      <button className="rounded border-gray-300 bg-gray-100 dark:bg-neutral-800/30 dark:border-neutral-700 hover:bg-gray-300 hover:dark:bg-neutral-700 px-5 py-4 transition-colors text-left">
         Submit
       </button>
     </div>
